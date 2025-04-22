15ba39031d07c65798fe3bfd64b448c693dfb619
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-04-22 17:03:32 +0200

    Small changes, not sure why dependabot is unhappy

--- src/main/java/tech/followthemoney/bahamut/resolver/Identifier.java
+++ src/main/java/tech/followthemoney/bahamut/resolver/Identifier.java
@@ -1,11 +1,15 @@
 package tech.followthemoney.bahamut.resolver;
 
+import java.math.BigInteger;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class Identifier implements Comparable<Identifier> {
     private static final String PREFIX = "NK-";
     private static final Pattern QID = Pattern.compile("^Q\\d+$");
+    private static final String ALPHABET = "23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    private static final BigInteger BASE57 = BigInteger.valueOf(57);
     protected final String id;
     private final int weight;
     private final boolean qid;
@@ -56,5 +60,30 @@ public class Identifier implements Comparable<Identifier> {
     public int compareTo(Identifier other) {
         int weightCompare = Integer.compare(this.weight, other.weight);
         return weightCompare != 0 ? weightCompare : this.id.compareTo(other.id);
+    }
+
+    public static Identifier generate() {
+        UUID uuid = UUID.randomUUID();
+        BigInteger number = new BigInteger(1, uuidToBytes(uuid));
+        
+        // Convert to Base57
+        StringBuilder identifier = new StringBuilder();
+        while (number.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger[] divmod = number.divideAndRemainder(BASE57);
+            identifier.insert(0, ALPHABET.charAt(divmod[1].intValue()));
+            number = divmod[0];
+        }
+        return new Identifier(PREFIX + identifier.toString());
+    }
+
+    private static byte[] uuidToBytes(UUID uuid) {
+        byte[] bytes = new byte[16];
+        long msb = uuid.getMostSignificantBits();
+        long lsb = uuid.getLeastSignificantBits();
+        for (int i = 0; i < 8; i++) {
+            bytes[i] = (byte) ((msb >> ((7 - i) * 8)) & 0xff);
+            bytes[i + 8] = (byte) ((lsb >> ((7 - i) * 8)) & 0xff);
+        }
+        return bytes;
     }
 }
